2d465405c519ae8b0b671e2393bbc3203d28a6c0
DixonRes <qimitu7@gmail.com>
2026-04-07 18:01:56 +0800

    Fix thread count: show actual used threads

--- dixon.c
+++ dixon.c
@@ -102,8 +102,8 @@ static void print_usage(const char *prog_name)
     printf("    -> No console output; solution file is still generated\n");
 
     printf("  Method selection:\n");
-    printf("    %s --method <method> <args>\n", prog_name);
-    printf("    -> Available methods: 1.Recursive; 2.Kronecker; 3.Interpolation\n");
+    printf("    %s --method <num> <args>\n", prog_name);
+    printf("    -> Available methods: 0.Recursive; 1.Kronecker; 2.Interpolation\n");
 
     printf("  Process count:\n");
     printf("    %s --threads <num> <args>\n", prog_name);
@@ -2386,10 +2386,17 @@ int main(int argc, char *argv[])
     double wall_time = (program_end.tv_sec - program_start.tv_sec) + 
                       (program_end.tv_usec - program_start.tv_usec) / 1000000.0;
 
-    /* ---- Get total threads first ---- */
+    /* ---- Get actual used threads ---- */
     int total_threads = 1;
     #ifdef _OPENMP
-    total_threads = omp_get_max_threads();
+    if (g_interpolation_threads > 0) {
+        total_threads = g_interpolation_threads;
+    } else {
+        total_threads = omp_get_max_threads();
+        // Default to half threads if not set
+        int half_threads = (total_threads + 1) / 2;
+        total_threads = half_threads;
+    }
     #endif
 
     /* ---- output results ---- */
